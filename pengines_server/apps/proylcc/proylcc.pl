b487fa2c5a0f11fdcb7e02e1776a615c8be1fe62
Katherina <katherinavilca@gmail.com>
2021-05-08 10:17:56 -0300

    Ideas sobre los predicados a implementar

--- pengines_server/apps/proylcc/proylcc.pl
+++ pengines_server/apps/proylcc/proylcc.pl
@@ -27,7 +27,10 @@ replace(X, XIndex, Y, [Xi|Xs], [Xi|XsY]):-
 put(Contenido, [RowN, ColN], _PistasFilas, _PistasColumnas, Grilla, NewGrilla, 0, 0):-
 	% NewGrilla es el resultado de reemplazar la fila Row en la posición RowN de Grilla
 	% (RowN-ésima fila de Grilla), por una fila nueva NewRow.
-	
+	 
+	%Si la pos es X o vacio verifica fila y columna si y solo el contenido es #.
+	%verificar_fila()
+
 	replace(Row, RowN, NewRow, Grilla, NewGrilla),
 
 	% NewRow es el resultado de reemplazar la celda Cell en la posición ColN de Row por _,
@@ -39,3 +42,22 @@ put(Contenido, [RowN, ColN], _PistasFilas, _PistasColumnas, Grilla, NewGrilla, 0
 	Cell == Contenido 
 		;
 	replace(_Cell, ColN, Contenido, Row, NewRow)).
+
+
+	%Verificar si se trata de una jugada valida si no se excedio el numero de pistas
+	%Si intento agregar un "#" en una fila/columna que ya está completa 
+	% (pueden quedar espacios blancos pero las pistas ya estan satisfechas) 
+	% debería quitar una vida y poner una cruz en el espacio blanco donde se intentó insertar el #
+	%Deberiamos recorrer la lista de pistas por Rown y por colN 
+	%Agregar: obtener_cant_pista_(+Posicion, + _Pistas,-Cant).
+	% Agregar: obtener_fila_grilla(+Grilla,+Posicion,-ListaFila).
+	%Agregar boton verificar.
+	%Agregar boton modo de juego( x o #).
+	%Agregar predicado a donde de forma recursiva va contando la cantidad de celdas pintadas.
+	% cant_celdas_pintadas(_ListaFila,+CantPistas,-CantPintadas)
+
+
+	verificar_fila(Contenido,Rown,-ListaFila,-Cant,-Flag):-  %Flag= true o false.
+	Contenido== "#" %Si es una celda a pintar.
+	%Llamar a predicado cant_celdas_pintadas, de ahi sacar la cantidad que estan pintadas
+	%Si cantidad de celdas pintadas es mayor a la cantidad de pistas de la fila return false. Pintar como invalida. Restar una vida.
