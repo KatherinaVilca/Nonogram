f80dfc89d94b616a1a5b077d6f11f481f4d6aa13
simonfredes <simonfredes7@gmail.com>
2021-05-10 13:27:20 -0300

    algoritmos de put

--- pengines_server/apps/proylcc/init.pl
+++ pengines_server/apps/proylcc/init.pl
@@ -13,3 +13,14 @@ init(
  [ _ , _ ,"#","#","#"]
 ]
 ).
+
+% colocar "#" en fila 2 columna 2  4rellenas en fila y  [1,3] en columna
+% si la posicion contiene una "X" entonces -> ¿?
+% si la posicion NO contiene una "X" entonces
+    % Obtener las pistas de la fila y las pistas de la columna
+    % Obtener estado actual de la grilla (obtener fila y columna donde se insertó)
+    %Verificar posicion valida -> si las pistas estan satisfechas entonces, poner una cruz (posicion invalida)
+    %                             si no -> si la posicion es invalida (originalmente no iría un "#" ahi) descontar valida
+    %                             si no -> marcar esa posicion con "#"
+    %                                      si marcamos y cumplimos todas las pistas -> marcar las blancas que queden (si quedan) con "X" y actualizar label de pistas de color verde
+    %                             retornar grilla actualizada
